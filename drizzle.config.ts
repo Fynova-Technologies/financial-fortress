78519682a74ac99e07248f0850353fb078dacc9d
bibashlearner <bibashchaudhary330@gmail.com>
2025-09-03 16:16:24 +0545

    change schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,11 +6,11 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./migrations",
-  schema: "./server/src/models/schema.ts",
+  schema: "./server/src/models/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     // url: `postgresql://postgres:bibash@localhost:5432/financial-fortress`,
-        host: "dpg-d2rtainfte5s739atuvg-a.oregon-postgres.render.com", 
+    host: "dpg-d2rtainfte5s739atuvg-a.oregon-postgres.render.com",
     port: 5432,
     user: "fynova_db_user",
     password: "mj2r0eglsJDd6N3OOk89REIc2fWXTIgU",
