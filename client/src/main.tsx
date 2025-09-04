137188574932923d6ed79ecfd855287b25eb908f
bibashlearner <bibashchaudhary330@gmail.com>
2025-09-04 14:54:12 +0545

    added auth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -11,10 +11,10 @@ console.log("domain: ", import.meta.env.VITE_AUTH0_DOMAIN)
 createRoot(document.getElementById("root")!).render(
   <Auth0Provider
   domain={import.meta.env.VITE_AUTH0_DOMAIN || "dev-l0cnkmnrn4reomjc.us.auth0.com"}
-    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID || "01MHsrRBXd0n3ewDmOLdjjqBpbKdpeNV"}
     authorizationParams={{
       redirect_uri: window.location.origin,
-      audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      audience: import.meta.env.VITE_AUTH0_AUDIENCE || "https://dev-l0cnkmnrn4reomjc.us.auth0.com/api/v2/",
       scope: "openid profile email",
     }}
   >
